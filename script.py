d30b0a866be0c065de6a4ea395edb9d4dea3013d
Charles Ivia <iviacharles@gmail.com>
2021-03-26 10:00:34 +0300

    Initial poetry construction fn

--- script.py
+++ script.py
@@ -8,10 +8,68 @@
 
 # Import required libraries
 import random
-from adj_scrapper import get_adjectives
+from scrapper import (
+    get_adjectives,
+    get_verbs,
+    get_un_nouns,
+    get_abstract_nouns,
+    get_collective_nouns,
+)
 
 adjectives = get_adjectives()
-random.shuffle(adjectives)
+verbs = get_verbs()
+linking_verbs = [
+    "am",
+    "is",
+    "are",
+    "was",
+    "were",
+    "has been",
+    "are being",
+    "might be",
+    "has been",
+    "could be",
+    "was being",
+    "has been",
+    "shall",
+]
+collective_nouns = get_collective_nouns()
+uncountable_nouns = get_un_nouns()
+abstract_nouns = get_abstract_nouns()
 
-chosen_adj = random.choice(adjectives)
-print(chosen_adj)
+nouns = uncountable_nouns + abstract_nouns
+articles = ["A", "An", "The"]
+
+
+def construct_poetry():
+    random.shuffle(adjectives)
+    selected_adjs = random.choices(adjectives, k=2)
+    random.shuffle(nouns)
+    selected_nouns = random.choices(nouns, k=2)
+    random.shuffle(verbs)
+    selected_verb = random.choice(verbs)
+    random.shuffle(linking_verbs)
+    selected_linking_verb = random.choice(linking_verbs)
+    selected_article = random.choices(articles, k=2)
+
+    print(
+        selected_article[1]
+        + " "
+        + selected_adjs[0]
+        + " "
+        + selected_nouns[0]
+        + " "
+        + selected_linking_verb
+        + " "
+        + selected_verb
+        + " "
+        + selected_article[1]
+        + " "
+        + selected_adjs[1]
+        + " "
+        + selected_nouns[1]
+        + "."
+    )
+
+
+construct_poetry()
